63e155f4a418d3211e6477c42b58392bfcd79a35
numvarn <phisan.shukkhi@gmail.com>
2018-02-27 20:53:30 +0700

    renamed:    08.wordVector/createWordVector.py -> 08.wordVector/03createWordVector.py 	new file:   08.wordVector/Stemming.py

---
+++ 08.wordVector/Stemming.py
@@ -0,0 +1,43 @@
+import csv
+import numpy as np
+
+class Stemming():
+    def __init__(self, input_file):
+        super(Stemming, self).__init__()
+        self.input_file = input_file
+
+    def stem(self):
+        rows = csv.reader(open(self.input_file))
+        line_count = 0
+        term_lt = []
+
+        for row in rows:
+            if line_count > 0:
+                term_lt.append([row[2], row[1]])
+            line_count += 1
+
+        term_arr = np.array(term_lt)
+        term_new = []
+        stem = []
+        for term in term_arr:
+            index = -1
+            if term[0] not in term_new:
+                term_new.append(term[0])
+                stem.append([term[0], term[1]])
+            else:
+                index = term_new.index(term[0])
+                line = stem[index]
+                stem[index] = line + [term[1]]
+
+        return stem
+
+
+if __name__ == '__main__':
+    in_file = "/Users/phisan/Desktop/WordListVerbs/original.csv"
+    stem = Stemming(in_file)
+    stemed = stem.stem()
+
+    number = 1
+    for line in stemed:
+        print("{} : {}".format(number, line))
+        number += 1
